39e4ab21b98aa78029b8401e5c11df65fe2ae595
Raghav Joshi <raghavjoshi079@gmail.com>
2020-05-07 22:39:23 +0530

    Added the top layer that introduces the customerwith it's name and has an image in the same row

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:
-      );
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25,left: 20.0),
+                    child: Text(
+                      "Hello, Raghav!",
+                      style: TextStyle(
+                        color: Color(0xFF537C61),
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25,right: 30.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(9.5),
+                      child: Image.asset(
+                        'images/pikachu.png',
+                        height: 40,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
