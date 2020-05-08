a21908b41df21778bbfd2bd40e3469029c67d2c5
Raghav Joshi <raghavjoshi079@gmail.com>
2020-05-08 09:49:21 +0530

    Added Icon and Text in second layer

--- lib/main.dart
+++ lib/main.dart
@@ -40,28 +40,44 @@ class MyApp extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
-                child: Container(
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Color(0xFF98DBAF),
-                    boxShadow: <BoxShadow>[],
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color(0xFF98DBAF),
+                        boxShadow: <BoxShadow>[],
+                      ),
+                    ),
+                    Positioned(
+                      top: 70 / 2,
+                      left: 20,
+                      child: Row(
                         children: <Widget>[
+                          Container(
+                            height: 80,
+                            child: Image.asset(
+                              'images/fruits/harvest.png',
+                              scale: 15,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFB7E6C7),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
                           Text(
                             'We picked up a new\nportion of fresh fruits\nfor you!',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20
-                            ),
+                            style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
